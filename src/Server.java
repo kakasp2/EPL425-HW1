7aff9f6a89cefc623d2797f4ac031bc2c2044e2a
acosta23 <acosta01@cs.ucy.ac.cy>
2016-10-02 20:09:22 +0300

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -5,19 +6,23 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
  
 public class Server
 {
  
     private static Socket socket;
-    private static byte[] ipAddr = new byte[] { (byte)192,(byte) 168,(byte) 56,(byte)1};
+    private static byte[] ipAddr;
     private static InetAddress address;
     private static ServerSocket serverSocket ;
-    private static int port = 25000;
+    private static int port = 25000,count=1,sum=0;
     private static int payload=(int)(Math.random() *1700)+300;
     private static String message="No message received";
     private static String parts[];
@@ -25,8 +30,15 @@ public class Server
     private static InputStream inStr;
     private static InputStreamReader isr;
     private static BufferedReader br ;
-    
-   private static  void sResponse(){
+    private static long startTime;
+    private static long endTime ;
+    private static long allTime;
+    private static double throughput;    
+    private static OperatingSystemMXBean myOsBean= 
+            ManagementFactory.getOperatingSystemMXBean();
+    private static List<String> list=new ArrayList<String>();
+  
+    private static  void sResponse(){
        try{
         OutputStream os = socket.getOutputStream();
         OutputStreamWriter osw = new OutputStreamWriter(os);
@@ -34,15 +46,19 @@ public class Server
         parts=message.split(" ");
       
         
-        if(parts.length==4){
-        	response="Welcome "+parts[3]+payload;
+        
+        	response="Welcome "+parts[3]+" "+payload;
             bw.write(response);
             bw.flush();
+          
+            
             System.out.println("Server said "+response);   
+            
+            count++;
             socket.close();
-        }
+      
         
-        
+
         
        }catch(IOException e){
     	   
@@ -51,12 +67,16 @@ public class Server
     
     private static void listener(){
     	try{
+    	startTime = System.currentTimeMillis();
+        
 	    socket = serverSocket.accept();
+	   // System.out.println("s "+startTime+" ");
 	    inStr = socket.getInputStream();
 	    isr = new InputStreamReader(inStr);
 	    br = new BufferedReader(isr);
 	     
 	    if(br.ready()){
+	        
 	       message = br.readLine();
 	       System.out.println("Client said: "+message );
 	       sResponse();
@@ -69,14 +89,36 @@ public class Server
     public static void main(String[] args)
     {
     	try{  
+    	String[] s=args[0].split("\\.");
+    	ipAddr = new byte[] { (byte)Integer.parseInt(s[0]),(byte)Integer.parseInt(s[1]),(byte)Integer.parseInt(s[2]),(byte)Integer.parseInt(s[3])};
     	address = InetAddress.getByAddress(ipAddr);
-		serverSocket = new ServerSocket(port,100000,address);
+		serverSocket = new ServerSocket(port,1000000,address);
 		}catch (Exception e){
 		e.printStackTrace();
 		}
-        System.out.println("Server Started and listening to the port "+port);
-        while(true)           
-        	listener();
+         System.out.println("Server Started and listening to the port "+port);
      
+     double load = myOsBean.getSystemLoadAverage();
+     OperatingSystemMXBean operatingSystemMXBean ;
+     long memory=0;
+        while(true){           
+        	listener();
+          endTime=System.currentTimeMillis();
+          allTime=allTime+(endTime-startTime);
+           throughput=allTime/count;
+           memory =memory+ Runtime.getRuntime().totalMemory() -  Runtime.getRuntime().freeMemory();
+           operatingSystemMXBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+           load=load+     operatingSystemMXBean.getSystemLoadAverage();
+          // System.out.println("load "+load);
+           System.out.println("memory "+memory);
+           System.out.println("Throughput is : "+throughput);
+            
+        	
+      
+        }
+        
+       
+          
+       
     }
 }
