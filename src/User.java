2253ea23830c8d350ae0b6775558f7800acd984d
acosta23 <acosta01@cs.ucy.ac.cy>
2016-10-02 20:10:17 +0300

    Update User.java

--- src/User.java
+++ src/User.java
@@ -6,9 +6,9 @@ public class User implements Runnable{
 
 	public void run(){
 		int count=0;
-		 while(count<800){
+		 while(count<300){
 			try {
-			new Client(InetAddress.getLocalHost(),(int) Thread.currentThread().getId());
+			new Client(InetAddress.getLocalHost(),(int) Thread.currentThread().getId());			
 			}catch (UnknownHostException e) {
 			e.printStackTrace();
 			}
@@ -28,10 +28,78 @@ public class User implements Runnable{
 	User user2=new User();
 	Thread u2=new Thread(user2);
 	u2.start();
+
 	User user3=new User();
 	Thread u3=new Thread(user3);
 	u3.start();
 	
+	User user4=new User();
+	Thread u4=new Thread(user4);
+	u4.start();
+
+	User user5=new User();
+	Thread u5=new Thread(user5);
+	u5.start();
+
+	User user6=new User();
+	Thread u6=new Thread(user6);
+	u6.start();
+
+	User user7=new User();
+	Thread u7=new Thread(user7);
+	u7.start();
+
+	User user8=new User();
+	Thread u8=new Thread(user8);
+	u8.start();
+
+	User user9=new User();
+	Thread u9=new Thread(user9);
+	u9.start();
+
+	User user10=new User();
+	Thread u10=new Thread(user10);
+	u10.start();
+	
+	/*User user11=new User();
+	Thread u11=new Thread(user6);
+	u11.start();
+
+	User user12=new User();
+	Thread u12=new Thread(user7);
+	u12.start();
+
+	User user13=new User();
+	Thread u13=new Thread(user8);
+	u13.start();
+
+	User user14=new User();
+	Thread u14=new Thread(user9);
+	u14.start();
+
+	User user15=new User();
+	Thread u15=new Thread(user10);
+	u15.start();
+	
+		User user16=new User();
+	Thread u16=new Thread(user6);
+	u16.start();
+
+	User user17=new User();
+	Thread u17=new Thread(user7);
+	u17.start();
+
+	User user18=new User();
+	Thread u18=new Thread(user8);
+	u18.start();
+
+	User user19=new User();
+	Thread u19=new Thread(user9);
+	u19.start();
+
+	User user20=new User();
+	Thread u20=new Thread(user10);
+	u20.start();*/
 	}
 
 }
